3c68f7e421c2cecd3f4b12ab3f8e4250e48c79dd
Hong Tang | Tester <77006112+hongtang-digiex@users.noreply.github.com>
2023-02-28 17:09:22 +0700

    floating number10

--- components/Animations/BackgroundAnimation.js
+++ components/Animations/BackgroundAnimation.js
@@ -1,55 +1,70 @@
-import React from "react";
-import { useState, useEffect } from "react";
+import React, {
+  useState,
+  useEffect,
+  useCallback,
+  useMemo,
+  useRef,
+} from "react";
 import NumberFloating from "./animate";
 
 export default function BackgroundAnimation() {
   const intervalGap = 400;
   const limit = 25;
   const [bubblePropsList, setBubblePropsList] = useState([]);
-  const colors = [
-    "#e97b40",
-    "#e9cbff",
-    "#99ccdd",
-    "#a4daef",
-    "#aaddee",
-    "#009489",
-    "#fdc8c0",
-    "#8de8e8",
-    "#ac92c0",
-    "#ab92b3",
-  ];
+  const colors = useMemo(
+    () => [
+      "#e97b40",
+      "#e9cbff",
+      "#99ccdd",
+      "#a4daef",
+      "#aaddee",
+      "#009489",
+      "#fdc8c0",
+      "#8de8e8",
+      "#ac92c0",
+      "#ab92b3",
+    ],
+    []
+  );
+
+  const generator = useCallback(() => {
+    const mainColor = colors[Math.floor(Math.random() * colors.length)];
+    setBubblePropsList((prev) => {
+      if (prev.length >= limit) {
+        clearInterval(intervalRef.current);
+      }
+      return [
+        ...prev,
+        {
+          mainColor,
+          number: Math.floor(Math.random() * 10),
+          styles: {
+            animationDuration: `${Math.random() * 6 + 10}s`,
+            top: 0,
+            left: `${Math.random() * 60 * 5}vw`,
+            transform: `scale(${Math.random() * 0.3 + 0.2})`,
+          },
+        },
+      ];
+    });
+  }, [colors]);
+
+  const intervalRef = useRef(null);
 
   useEffect(() => {
-    const generator = setInterval(() => {
-      const mainColor = colors[Math.floor(Math.random() * colors.length)];
-      setBubblePropsList((prev) => {
-        if (prev.length >= limit) {
-          clearInterval(generator);
-        }
-        return [
-          ...prev,
-          {
-            mainColor,
-            number: Math.floor(Math.random() * 10),
-            styles: {
-              animationDuration: `${Math.random() * 6 + 10}s`,
-              top: 0,
-              left: `${Math.random() * 60 * 5}vw`,
-              transform: `scale(${Math.random() * 0.3 + 0.2})`,
-            },
-          },
-        ];
-      });
-    }, intervalGap);
-  }, []);
+    intervalRef.current = setInterval(generator, intervalGap);
+    return () => clearInterval(intervalRef.current);
+  }, [generator]);
 
-  return (
-    <div className="fixed -top-23 animate-frame">
-      {bubblePropsList.map((bubble, index) => (
-        <div key={index} className="absolute circle-dot" style={bubble.styles}>
-          <NumberFloating number={bubble.number} mainColor={bubble.mainColor} />
-        </div>
-      ))}
-    </div>
-  );
+  const bubbles = [];
+  for (let i = 0; i < bubblePropsList.length; i++) {
+    const bubble = bubblePropsList[i];
+    bubbles.push(
+      <div key={i} className="absolute circle-dot" style={bubble.styles}>
+        <NumberFloating number={bubble.number} mainColor={bubble.mainColor} />
+      </div>
+    );
+  }
+
+  return <div className="fixed -top-23 animate-frame">{bubbles}</div>;
 }
