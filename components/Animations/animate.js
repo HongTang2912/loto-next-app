b02b7c919202f53c9f87dcbab726aa48e53d1723
hongtang-digiex <hongtang240@gmail.com>
2023-05-19 16:39:06 +0700

    animation new

--- components/Animations/animate.js
+++ components/Animations/animate.js
@@ -1,30 +1,55 @@
-import React from "react";
+import React, { useRef } from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 
 export default function NumberFloating({ number, mainColor, blobStyle }) {
+  const Behind = useRef(null);
+  const sizes = [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
+  const [streak1, setStreak1] = useState(null);
+ const [streak2, setStreak2] = useState(null);
+  const [streak3, setStreak3] = useState(null);
+  const intervalGenerator = (cb) => {
+     const streakSize = sizes[Math.floor(Math.random() * (sizes.length - 1))];
+    cb(
+      <span
+        style={{
+          borderColor: mainColor,
+          height: streakSize,
+          width: streakSize,
+          left: `${Math.floor(Math.random() * 100)}%`,
+        }}
+        className={`absolute  border-4 rounded-full fade`}
+      ></span>
+    );
+       
+  };
+
+  useEffect(() => {
+    setInterval(() => intervalGenerator(setStreak1), 1200);
+    setTimeout(() => {
+      setInterval(() => intervalGenerator(setStreak2), 1200);
+
+    },400)
+    setTimeout(() => {
+      setInterval(() => intervalGenerator(setStreak3), 1200);
+    }, 800);
+  }, []);
+
   return (
     <span
       className="inline-flex flex-col gap-5 scaler"
       style={{ color: mainColor }}
     >
-      <span className="relative flex h-32 justify-evenly ">
-        <div>
+      <span className="relative flex h-32 justify-evenly " ref={Behind}>
+        {/* <div>
           <span
-            style={{ borderColor: mainColor }}
-            className="absolute h-20 border-4 rounded-full fade-delay-1 fade"
+            style={{ borderColor: mainColor, height: size, width: size, left:'80%' }}
+            className={`absolute  border-4 rounded-full fade`}
           ></span>
-        </div>
-        <div>
-          <span
-            style={{ borderColor: mainColor }}
-            className="absolute h-20 border-4 rounded-full fade"
-          ></span>
-        </div>
-        <div>
-          <span
-            style={{ borderColor: mainColor }}
-            className="absolute h-20 border-4 rounded-full bg fade-delay-2 fade"
-          ></span>
-        </div>
+        </div> */}
+        {streak1}
+        {streak2}
+        {streak3}
       </span>
 
       <div
---
+++ pages/animate.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import NumberFloating from '../components/Animations/animate'
+import { blobs } from "../utils/configuration/blobs";
+
+export default function animate() {
+  return (
+    <div>
+      <NumberFloating
+        number="H"
+        mainColor="#000"
+        blobStyle={blobs[0]}
+      />
+    </div>
+  );
+}
